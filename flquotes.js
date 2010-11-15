f88d4d00b72e02d500f443ed77196561f19cdc9a
chairface <jciarlan@gmail.com>
2010-11-14 23:54:28 -0500

    removed stuff that wasnt being used

--- flquotes.js
+++
@@ -1,6 +0,0 @@
-var css = ".quoted-message{\n	margin-left: 15px;\n padding-left: 0px;\n border: 4px solid;\n -webkit-border-radius: 4px;\n}\n}";
-var heads = document.getElementsByTagName("head");
-var node = document.createElement("style");
-node.type = "text/css";
-node.appendChild(document.createTextNode(css));
-heads[0].appendChild(node);
